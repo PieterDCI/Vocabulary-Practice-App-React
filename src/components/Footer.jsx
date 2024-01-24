2dad4918e20d473cfe95d5953b48bb20f9c6c186
Pieter <pieter.heethem@dci-student.org>
2024-01-24 10:10:59 +0100

    add controlled inputs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,23 @@
 import "../styling/footer.css"
 
+const aStyle = {
+    color: 'white',
+    marginLeft: '5px'
+}
+
 function Footer() {
     return (
-        <footer>This is a footer</footer>
+        <footer style={{display: 'flex', justifyContent: 'space-between', fontSize: '1.2rem'}}>
+            <p><i class="icon-code"></i> Link to Code</p>
+            <p>All rights reserved 2024</p>
+            <nav >
+                <ul style={{display: 'flex', textDecoration: 'none', color: 'white'}}>
+                   <li><a href="#" style={aStyle}><i className="icon-info-sign"></i> T&C</a></li>
+                   <li><a href="#" style={aStyle}><i className="icon-question-sign"></i> FAQ </a></li>
+                   <li><a href="#" style={aStyle}><i className="icon-comment"></i> Contact</a></li>
+                </ul>
+            </nav>
+        </footer>
     )
 }
 
--- src/components/modes/AddMode.jsx
+++ src/components/modes/AddMode.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import "../../styling/inputs.css";
+import "../../styling/buttons.css";
+
 function AddMode() {
+
+    const [noun, setNoun] = useState('');
+    const [article, setArticle] = useState('');
+    const [plural, setPlural] = useState('');
+    const [translation, setTranslation] = useState('');
+
+    const changeNoun = (e) => setNoun(e.target.value);
+    const changeArticle = (e) => setArticle(e.target.value);
+    const changeTranslation = (e) => setTranslation(e.target.value);
+    const changePlural = (e) => setPlural(e.target.value);
+
+    const addWordToStorage = () => {
+        return (null)
+    }
+
     return(
         <main>
             <div className="wrapper">
-                <p>Add mode</p>
+                <input type="text" placeholder="Enter Noun" value={noun} onChange={changeNoun}/>
+                <input type="text" placeholder="Enter Article" value={article} onChange={changeArticle} />
+                <input type="text" placeholder="Enter Plural" value={plural} onChange={changePlural} />
+                <input type="text" placeholder="Enter Translation" value={translation} onChange={changeTranslation} />
+                <button onClick={addWordToStorage} style={{marginTop: '15px'}}><i className="icon-plus"></i></button>
             </div>
        </main>
     )
